f57fd4202d202217aba3a15c87cfb531e069ce10
jorgedelcampo <j.delcampo.a@gmail.com>
2023-01-06 06:59:48 -0300

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -15,6 +15,7 @@ let skinIndex = 0;
 let player = {
     init: function() {
         player.serviceWorker();
+        player.installPWA();
         player.lockOrientation();
         //player.mediaControls();
         player.loadSongs();
@@ -30,6 +31,28 @@ let player = {
                 .catch(err => console.log("service worker not registered", err))
             })
           }
+    },
+    
+    installPWA: function(){
+        let deferredPrompt;
+        window.addEventListener('beforeinstallprompt', (e) => {
+          // Prevent Chrome 67 and earlier from automatically showing the prompt
+          e.preventDefault();
+          // Stash the event so it can be triggered later.
+          deferredPrompt = e;
+        });
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+          .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+              console.log('User accepted the A2HS prompt');
+            } else {
+              console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+          });
     },
 
     lockOrientation: function() {
