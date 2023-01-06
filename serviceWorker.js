5b8d9783dc94a88de58cebbdeeedad4592913223
jorgedelcampo <j.delcampo.a@gmail.com>
2023-01-06 07:13:14 -0300

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,18 @@
 const staticMediaPlayer = "custom-media-player-v1"
 const assets = [
-  //"/",
-  "index.html",
-  "assets/css/style.css",
-  "assets/js/app.js",
-  "assets/img/cassette.png",
-  "assets/img/icon.svg",
-  "assets/img/noise.png",
-  "assets/img/vinyl.svg",
-  "assets/svg/add.svg",
-  "assets/svg/list.svg",
-  "assets/svg/media-control-next.svg",
-  "assets/svg/media-control-pause.svg",
-  "assets/svg/media-control-play.svg",
+  "/",
+  "/index.html",
+  "/assets/css/style.css",
+  "/assets/js/app.js",
+  "/assets/img/cassette.png",
+  "/assets/img/icon.svg",
+  "/assets/img/noise.png",
+  "/assets/img/vinyl.svg",
+  "/assets/svg/add.svg",
+  "/assets/svg/list.svg",
+  "/assets/svg/media-control-next.svg",
+  "/assets/svg/media-control-pause.svg",
+  "/assets/svg/media-control-play.svg",
 ]
 
 self.addEventListener("install", installEvent => {
